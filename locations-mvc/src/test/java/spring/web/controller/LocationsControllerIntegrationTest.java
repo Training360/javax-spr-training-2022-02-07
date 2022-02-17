dd43384cf5aeb749d227eb972362530e2c1c12ce
Istvan Viczian <viczian.istvan@gmail.com>
2022-02-17 13:40:20 +0100

    Teszteset a fájlfeltöltésre

--- locations-mvc/src/test/java/spring/web/controller/LocationsControllerIntegrationTest.java
+++ locations-mvc/src/test/java/spring/web/controller/LocationsControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
@@ -11,10 +12,14 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import spring.web.backend.BackendConfig;
 import spring.web.backend.LocationService;
+import spring.web.model.Location;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItem;
@@ -45,15 +50,15 @@ public class LocationsControllerIntegrationTest {
 
     @Test
     public void testSaveThenListLocations() throws Exception {
-        MockMultipartFile testFile = new MockMultipartFile("data", "filename.svg",
-                "image/svg", "example svg".getBytes());
+        MockMultipartFile testFile = new MockMultipartFile("file", "filename.svg",
+                "image/svg", new ClassPathResource("/spring.svg", LocationsControllerIntegrationTest.class).getInputStream());
 
         mockMvc.perform(multipart("/")
                 .file(testFile)
                 .param("name", "Budapest")
                 .param("coordinates", "34.62, 12.87"));
 
-        mockMvc.perform(get("/"))
+        MvcResult mvcResult = mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
                 .andExpect(model().attribute("locations",
@@ -62,6 +67,12 @@ public class LocationsControllerIntegrationTest {
                         hasItem(hasProperty("lat", equalTo(34.62)))))
                 .andExpect(model().attribute("locations",
                         hasItem(hasProperty("lon", equalTo(12.87)))))
-                .andExpect(content().string(containsString("Budapest")));
+                .andExpect(content().string(containsString("Budapest")))
+                .andReturn();
+
+        long id = ((List<Location>) mvcResult.getModelAndView().getModel().get("locations")).get(0).getId();
+
+        mockMvc.perform(get("/images/{id}", id))
+                .andExpect(status().isOk());
     }
 }
