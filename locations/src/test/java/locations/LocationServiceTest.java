a5495d7c29846ba9fcd8fc5e19d9625d318b7620
Istvan Viczian <viczian.istvan@gmail.com>
2022-02-17 09:25:20 +0100

    Unitteszt a service-re.

--- locations/src/test/java/locations/LocationServiceTest.java
+++ locations/src/test/java/locations/LocationServiceTest.java
@@ -1,22 +1,30 @@
 package locations;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class LocationServiceTest {
 
-    private LocationDao locationDao = new ListLocationDao();
-    private LocationService locationService = new LocationService(locationDao);
+    @Mock
+    private LocationDao locationDao;
+
+    @InjectMocks
+    private LocationService locationService;
 
     @Test
-    public void saveThenList() {
+    public void testSave() {
         locationService.createLocation("BP", 1.0, 1.0);
-        locationService.createLocation("SP", 2.5, 1.5);
-        assertEquals(Arrays.asList("BP", "SP"), locationService.listLocations().stream()
-                .map(Location::getName).collect(Collectors.toList()));
+        verify(locationDao).save("BP", 1.0, 1.0);
     }
 }
