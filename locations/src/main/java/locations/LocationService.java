1c7f60d7fa71a4e2a3f1566396f425b002028bf1
Istvan Viczian <viczian.istvan@gmail.com>
2022-02-17 10:12:02 +0100

    Nem kell külön ApplicationEventPublisher.

--- locations/src/main/java/locations/LocationService.java
+++ locations/src/main/java/locations/LocationService.java
@@ -2,9 +2,7 @@ package locations;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,7 +13,6 @@ public class LocationService {
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private ApplicationContext applicationContext;
     private final LocationDao locationDao;
-    private ApplicationEventPublisher applicationEventPublisher;
 
     public LocationService(ApplicationContext applicationContext, LocationDao locationDao) {
         this.applicationContext = applicationContext;
@@ -40,22 +37,19 @@ public class LocationService {
 
     public Optional<Location> updateLocation(long id, String name, double lat, double lon) {
         logger.debug("Item updated with new values: {}, {}, {}", name, lat, lon);
-        LocationMemento oldLocation = new LocationMemento(getLocationById(id).orElse(null));
-        Optional<Location> location = locationDao.update(id, name, lat, lon);
-        if(applicationEventPublisher != null && location.isPresent()) {
-            LocationMemento newLocation = new LocationMemento(location.orElse(null));
+        Optional<Location> found = getLocationById(id);
+        if (found.isPresent()) {
+            LocationMemento oldLocation = new LocationMemento(getLocationById(id).get());
+            locationDao.update(id, name, lat, lon);
+            LocationMemento newLocation = new LocationMemento(found.get());
             LocationHasChangedEvent event = new LocationHasChangedEvent(this, oldLocation, newLocation);
-            applicationEventPublisher.publishEvent(event);
+            applicationContext.publishEvent(event);
         }
-        return location;
+        return found;
     }
 
     public Optional<Location> deleteLocation(long id) {
         return locationDao.delete(id);
     }
 
-    @Autowired(required = false)
-    public void  setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-        this.applicationEventPublisher = applicationEventPublisher;
-    }
 }
